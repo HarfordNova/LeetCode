30cd28d9950a271a8220d90c09107ae86c72cd37
Harford <harfordnova@gmail.com>
2019-03-26 11:19:57 +0800

    solve problem 1: tow sum

---
+++ 1.two-sum.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (42.68%)
+ * Total Accepted:    1.6M
+ * Total Submissions: 3.7M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // HashMap 初始化时设置大小，可以将运行时间从3ms缩短到1ms
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        //用Map缓存数组的值，就只需要一次循环
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}

48d0fe0735a9d7de2ea9feda26c0c143b4ff62f8
Harford <harfordnova@gmail.com>
2019-03-26 17:26:46 +0800

    solution 2 for problem 2

--- 2.add-two-numbers.java
+++ 2.add-two-numbers.java
@@ -32,31 +32,65 @@
  * next; ListNode(int x) { val = x; } }
  */
 class Solution {
-    //首先想到的方案就是一位一位相加
+    // 方案二
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int sum = l1.val + l2.val;
-        //保留队列头
         ListNode head = new ListNode(sum % 10);
         ListNode l3N = head;
         ListNode n1 = l1.next;
         ListNode n2 = l2.next;
-        //有任何一个列表还有元素或者有进位都需要再往后进行加操作
-        while (n1 != null || n2 != null || sum / 10 > 0) {
-            //队列没有元素的话，设为0
+        // 相比方案一少做进位的循环判断
+        while (n1 != null || n2 != null) {
             int a = n1 == null ? 0 : n1.val;
             int b = n2 == null ? 0 : n2.val;
-            //sum/10等于十位数，也就是进位的值
             sum = a + b + sum / 10;
-            ListNode l3Next = new ListNode(sum % 10);
-            l3N.next = l3Next;
-            //为下一个循环做准备
-            l3N = l3Next;
+            int val = sum % 10;
+            //去掉Node的引用，节省空间（多次提交，内存消耗不一样，因此感觉LeetCode内存计算不准确）
+            l3N.next = new ListNode(val);
+            l3N = l3N.next;
             if (n1 != null)
                 n1 = n1.next;
             if (n2 != null)
                 n2 = n2.next;
         }
 
-        return l3;
+        if (sum / 10 > 0)
+            l3N.next = new ListNode(sum / 10);
+
+        return head;
     }
 }
+
+
+
+
+
+// class Solution {
+//     //首先想到的方案就是一位一位相加
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         int sum = l1.val + l2.val;
+//         //保留队列头
+//         ListNode head = new ListNode(sum % 10);
+//         ListNode l3N = head;
+//         ListNode n1 = l1.next;
+//         ListNode n2 = l2.next;
+//         //有任何一个列表还有元素或者有进位都需要再往后进行加操作
+//         while (n1 != null || n2 != null || sum / 10 > 0) {
+//             //队列没有元素的话，设为0
+//             int a = n1 == null ? 0 : n1.val;
+//             int b = n2 == null ? 0 : n2.val;
+//             //sum/10等于十位数，也就是进位的值
+//             sum = a + b + sum / 10;
+//             ListNode l3Next = new ListNode(sum % 10);
+//             l3N.next = l3Next;
+//             //为下一个循环做准备
+//             l3N = l3Next;
+//             if (n1 != null)
+//                 n1 = n1.next;
+//             if (n2 != null)
+//                 n2 = n2.next;
+//         }
+
+//         return head;
+//     }
+// }
